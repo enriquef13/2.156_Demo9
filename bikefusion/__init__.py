9a6af2e602cbb8566989e52b5bb070b99153510d
Enrique Flores <enriquefloresm@berkeley.edu>
2024-11-24 13:02:54 -0500

    Remove data and bikefusion

--- bikefusion/__init__.py
+++
@@ -1,9 +0,0 @@
-from .bikefusion import *
-import os
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-def load_bikefusion():
-    return load_bikefusion_and_data(current_dir)
-
-__all__ = [name for name in dir() if not name.startswith("_") and name not in {"os"}]
--- bikefusion/bikefusion/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_utils import load_data, preprocess, postprocess, to_mask_map, load_bikefusion_and_data
-from .visualizers import visualize_imagesets, visualize_image_evolution
-from .pipline import InpaintingDenoisingPipeline
-from .diffusion import InpaintingDenoisingDiffusion
